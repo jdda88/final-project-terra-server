c6752db2842deb99d7a5a235ca9c41b8e51f0a88
annafricks <annafricks33@gmail.com>
2024-06-21 09:54:14 -0400

    comment ffrom daniel

--- models/review.model.js
+++ models/review.model.js
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 // ;;daniel was here
+//another comment from daniel
 const reviewSchema = new Schema(
   {
     title: { type: String, required: true, trim: true, maxLength: 64 },
